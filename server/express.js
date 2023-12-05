d33ff6fd567dea3d8e8e5924cb337bbf7ecaa4ff
Dom1232 <bjames33@my.centennialcollege.ca>
2023-12-05 07:21:46 -0500

    Changed Cors policy to accept new origin

--- server/express.js
+++ server/express.js
@@ -12,7 +12,7 @@ const path = require('path');
 
 const app = express();
 const CURRENT_WORKING_DIR = process.cwd();
-app.use(cors({ credentials: true, origin: 'http://localhost:8080' }));
+app.use(cors({ credentials: true, origin: 'https://nexus-11h5.onrender.com' }));
 
 //...
 app.use(express.static(path.join(CURRENT_WORKING_DIR, 'dist/app')));
