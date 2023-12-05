06bdffdf58c35153cfb1e64855532fe5c1987843
Dom1232 <bjames33@my.centennialcollege.ca>
2023-12-05 07:12:58 -0500

    Updated vue config

--- client/vue.config.js
+++ client/vue.config.js
@@ -6,7 +6,7 @@ module.exports = defineConfig({
   transpileDependencies: true,
   outputDir: '../dist/app',
 
-  server: {
+  devServer: {
     proxy: {
       '/api': {
         target: `http://localhost:${PORT}`,
